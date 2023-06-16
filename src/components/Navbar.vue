6c6740bf38e325966a805b62705da3c873673b1a
Agustin-Graciano <agusnachogracianox@gmail.com>
2023-06-17 00:11:10 +0200

    Reverse

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-end bg-black py-6">
       <router-link 
               to='/SignUp' 
-              class="text-white mr-6 hover:text-eg-blue cursor-pointer">Sign Ups
+              class="text-white mr-6 hover:text-eg-blue cursor-pointer">Sign Up
       </router-link>
     </div>
   </div>
