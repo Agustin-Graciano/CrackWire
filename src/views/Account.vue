6df0b00ea6bee7bd4334829baa6dd02224379df1
Agustin-Graciano <agusnachogracianox@gmail.com>
2023-06-23 03:06:25 +0200

    Username Account addition

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,25 +1,30 @@
 <template>
     <div class="flex flex-col min-h-screen">
       <Navbar />
-      <div class="flex flex-col bg-eg-blue">
+      <div class="flex flex-col bg-eg-blue z-10">
+        <!-- User Info Section -->
         <div class="flex justify-start h-24">
-          <div class="w-48 pt-2 mx-5">
+          <div class="w-48 py-5 mx-10">
             <img
-              src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-              alt="..."
-              class="align-middle z-30 object-cover w-32 h-32 border-2
-               border-white rounded-full hover:border-eg-blue transition duration-300"
+              @click="addProfilePicture"
+               id="profilePicture"
+               alt=""
+               :src="profilePicture || 'https://www.pragyansws.org/uploads/image/pragyan/icon.png'"
+               class="align-middle z-30 object-cover w-32 h-32 border-2 border-white bg-white rounded-full hover:border-eg-blue transition duration-300 cursor-pointer"
             />
+
           </div>
           <div class="flex flex-col justify-center">
-            <strong class="font-semibold text-xl text-white">Username</strong>
-            <span class="font-semibold text-md text-white">Status:</span>
+            <strong class="font-semibold text-xl text-white -mx-16">Username</strong>
+            <span class="font-semibold text-md text-white -mx-16">Status:</span>
           </div>
         </div>
       </div>
+      <!-- Posted Messages Section -->
       <div class="flex-grow flex bg-dark-grey">
         <h1 class="text-xl font-semibold text-white mt-5 mx-48">Posted Messages:</h1>
       </div>
+      <!-- Display your messages here -->
     </div>
   </template>
   
@@ -30,8 +35,54 @@
     components: {
       Navbar,
     },
-  };
-  </script>
+    data() {
+     return {
+       profilePicture: '/https://www.pragyansws.org/uploads/image/pragyan/icon.png',
+     };
+    },
+
+    methods: {
+    // Adds profile picture
+    addProfilePicture() {
+      const fileInput = document.createElement('input');
+      fileInput.type = 'file';
+      fileInput.accept = 'image/*';
+      fileInput.onchange = (event) => {
+        const file = event.target.files[0];
+
+        const reader = new FileReader();
+        reader.onload = () => {
+          const imageDataUrl = reader.result;
+
+          // Save the picture to Local Storage
+          this.saveProfilePicture(imageDataUrl);
+
+          // Update the profile picture display
+          const profilePicture = document.getElementById('profilePicture');
+          profilePicture.src = imageDataUrl;
+        };
+        reader.readAsDataURL(file);
+      };
+
+      // Trigger the file input dialog
+      fileInput.click();
+    },
+    //Saves profile picture
+    saveProfilePicture(imageDataUrl) {
+      localStorage.setItem('profilePicture', imageDataUrl);
+    },
+  },
+  mounted() { // The mounted lifecycle hook is a function that is executed after the component is inserted into the DOM (Document Object Model). 
+             // It is a part of the component lifecycle in Vue.js.
+    // Load the profile picture from Local Storage, if available
+    const savedPicture = localStorage.getItem('profilePicture');
+    if (savedPicture) {
+      const profilePicture = document.getElementById('profilePicture');
+      profilePicture.src = savedPicture;
+    }
+  },
+}; 
+</script>
   
   <style scoped>
   .flex {
@@ -62,7 +113,7 @@
   }
   
   .hover\:border-eg-blue:hover {
-    border-color: #0ea5e9;
+    border-color: #000dff;
   }
   
   .transition {
